6d817c793f8477632dae4f07bc7185abb711fb24
uRHL <34071457+uRHL@users.noreply.github.com>
2017-12-07 13:03:02 +0100

    Rename Board.java to src/structures/Board.java

--- src/structures/Board.java
+++ src/structures/Board.java
@@ -3,7 +3,7 @@ package structures;
 /**
  * Unimplemented class in this version
  * 
- * @author RamÛn Hern·ndez LeÛn. Bachelor Degree in Computer Science. UC3M
+ * @author Ram√≥n Hern√°ndez Le√≥n. Bachelor Degree in Computer Science. UC3M
  * @since December, 6, 2017
  * @version 1.1
  */
